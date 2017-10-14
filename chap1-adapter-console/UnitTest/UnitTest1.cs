b365aff251ac6fcd2b9f66255f45490a1a53484d
Chris Cha <chris.cha@live.ca>
2017-10-14 16:05:57 -0400

    init version

---
+++ chap1-adapter-console/UnitTest/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Moq;
+using chap1_adapter_console.HelloDI.CommandLine;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ExclaimWillWriteCorrectMessageToMessageWriter()
+        {
+            var writeMock = new Mock<IMessageWriter>();
+            var sut = new Salutation(writeMock.Object);
+            sut.Exclaim();
+            writeMock.Verify(w => w.Write("Hello DI!"));
+        }
+    }
+}
